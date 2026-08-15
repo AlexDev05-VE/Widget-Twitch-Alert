3b7eed8044ae0546c3d2fa08e440a93995ae23ad
AlexDev05 <alexisgudino2002@gmail.com>
2026-08-15 00:49:50 -0400

    Se implemento xbox_sound_bits.js, una funcionalidad completa sobre abstracciones y herencia para clases concretas que ayudaran al uso de Patrones de Diseño Abstract Factory

---
+++ src/frontend/js/module/sound/xbox_sound_bits.js
@@ -0,0 +1,83 @@
+import { Bits } from "./sound_xbox.js";
+
+
+class ObsidianBitsSound extends Bits {
+    get getNameSound() {
+        return { sonido: "Obsidian Bits Theme - Xbox" };
+    }
+    get getTypeAlert() {
+        return { sonido: "Bits" };
+    }
+    get getSrcSound() {
+        return { sonido_src: "../assets/sound/xbox_sound/xbox_logro.mp3" };
+    }
+}
+
+class PurpleBitsSound extends Bits {
+    get getNameSound() {
+        return { sonido: "Purple Bits Theme - Xbox" };
+    }
+    get getTypeAlert() {
+        return { sonido: "Bits" };
+    }
+    get getSrcSound() {
+        return { sonido_src: "../assets/sound/xbox_sound/xbox_logro.mp3" };
+    }
+}
+
+class EsmeraldaBitsSound extends Bits {
+    get getNameSound() {
+        return { sonido: "Esmeralda Bits Theme - Xbox" };
+    }
+    get getTypeAlert() {
+        return { sonido: "Bits" };
+    }
+    get getSrcSound() {
+        return { sonido_src: "../assets/sound/xbox_sound/xbox_logro.mp3" };
+    }
+}
+
+class DiamanteBitsSound extends Bits {
+    get getNameSound() {
+        return { sonido: "Diamante Bits Theme - Xbox" };
+    }
+    get getTypeAlert() {
+        return { sonido: "Bits" };
+    }
+    get getSrcSound() {
+        return { sonido_src: "../assets/sound/xbox_sound/xbox_logro.mp3" };
+    }
+}
+
+class RubiBitsSound extends Bits {
+    get getNameSound() {
+        return { sonido: "Rubi Bits Theme - Xbox" };
+    }
+    get getTypeAlert() {
+        return { sonido: "Bits" };
+    }
+    get getSrcSound() {
+        return { sonido_src: "../assets/sound/xbox_sound/xbox_logro.mp3" };
+    }
+}
+
+class AmbarBitsSound extends Bits {
+    get getNameSound() {
+        return { sonido: "Ambar Bits Theme - Xbox" };
+    }
+    get getTypeAlert() {
+        return { sonido: "Bits" };
+    }
+    get getSrcSound() {
+        return { sonido_src: "../assets/sound/xbox_sound/xbox_logro.mp3" };
+    }
+}
+
+export {
+    ObsidianBitsSound,
+    PurpleBitsSound,
+    EsmeraldaBitsSound,
+    DiamanteBitsSound,
+    RubiBitsSound,
+    AmbarBitsSound
+}
